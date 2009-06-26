754348d34fe1c6643d1a474bb90de1f5bea2a6b4
okkez <okkez000@gmail.com>
2009-06-26 18:54:43 +0900

    handle error when connect to emacswiki

--- emacs-config/config-auto-install.el
+++ emacs-config/config-auto-install.el
@@ -1,4 +1,6 @@
 (require 'auto-install)
 (setq auto-install-directory "~/.elisp/repos/")
-(auto-install-update-emacswiki-package-name t)
+(condition-case nil
+    (auto-install-update-emacswiki-package-name t)
+  (error (message "couldn't update package name, ignore error...")))
 (auto-install-compatibility-setup)
