4ad8dcc0571a79111114de67e596b97c3f6af1fc
okkez <okkez000@gmail.com>
2009-07-14 23:14:15 +0900

    now can show holidays in calendar

--- emacs-config/config-misc.el
+++ emacs-config/config-misc.el
@@ -90,5 +90,21 @@
 (autoload 'racc-mode "racc-mode" "alternate mode for editing racc files" t)
 (setq auto-mode-alist (append '(("\\.ry$" . racc-mode)) auto-mode-alist))
 
+;; calendar show holidays
+(require 'calendar)
+(setq  number-of-diary-entries 31)
+(define-key calendar-mode-map "f" 'calendar-forward-day)
+(define-key calendar-mode-map "n" 'calendar-forward-day)
+(define-key calendar-mode-map "b" 'calendar-backward-day)
+(setq mark-holidays-in-calendar t)
+;; (install-elisp "http://www.meadowy.org/meadow/netinstall/export/799/branches/3.00/pkginfo/japanese-holidays/japanese-holidays.el")
+(require 'japanese-holidays)
+(setq calendar-holidays
+      (append japanese-holidays local-holidays other-holidays))
+(setq calendar-weekend-marker 'diary)
+(add-hook 'today-visible-calendar-hook 'calendar-mark-weekend)
+(add-hook 'today-invisible-calendar-hook 'calendar-mark-weekend)
+
+
 ;; emacsclient
 (server-start)
