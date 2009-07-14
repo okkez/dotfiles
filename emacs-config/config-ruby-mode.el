4d97efbfb9ce44b29f0f1eae87a181e68e7e2daf
okkez <okkez000@gmail.com>
2009-07-15 00:18:59 +0900

    open not writable file as view-mode

--- emacs-config/config-ruby-mode.el
+++ emacs-config/config-ruby-mode.el
@@ -54,7 +54,9 @@
  'ruby-mode-hook
  '(lambda ()
     ;; Don't want flymake mode for ruby regions in rhtml files
-    (if (not (null buffer-file-name)) (flymake-mode))
+    (if (and (not (null buffer-file-name))
+             (file-writable-p (file-name-directory buffer-file-name)))
+        (flymake-mode))
     ;; エラー行で C-c d するとエラーの内容をミニバッファで表示する
     (define-key ruby-mode-map "\C-cd" 'credmp/flymake-display-err-minibuf)))
 
