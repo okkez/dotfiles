38050037b8b361d092e99456b699581815a5e7c7
okkez <okkez000@gmail.com>
2010-03-25 22:07:00 +0900

    add haml-mode

---
+++ emacs-config/config-haml-mode.el
@@ -0,0 +1,9 @@
+;; haml-mode
+;; git://github.com/nex3/haml.git
+(add-to-list 'load-path "~/.elisp/haml/extra")
+(require 'haml-mode)
+(require 'sass-mode)
+
+(add-to-list 'ac-modes 'haml-mode)
+(add-to-list 'ac-modes 'sass-mode)
+(add-hook 'sass-mode-hook 'ac-css-keywords-setup)
