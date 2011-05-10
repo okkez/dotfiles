7adaae4d1fff01123d4bd9aedccf1903f01c1740
Kenji Okimoto <okimoto@clear-code.com>
2011-05-10 10:03:15 +0900

    add increment number at point.

--- emacs-config/config-misc.el
+++ emacs-config/config-misc.el
@@ -132,5 +132,32 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 
+;; カーソル位置の数字をインクリメント
+(defun increment-string-as-number (number)
+  "Replace progression string of the position of the cursor
+by string that added NUMBER.
+Interactively, NUMBER is the prefix arg.
+
+examle:
+At the cursor string \"12\"
+
+M-x increment-string-as-number ;; replaced by \"13\"
+C-u 10 M-x increment-string-as-number ;; replaced by \"22\"
+
+At the cursor string \"-12\"
+
+M-x increment-string-as-number ;; replaced by \"-11\"
+C-u 100 M-x increment-string-as-number ;; replaced by \"88\""
+  (interactive "P")
+  (let ((col (current-column))
+        (p (if (integerp number) number 1)))
+    (skip-chars-backward "-0123456789")
+    (or (looking-at "-?[0123456789]+")
+        (error "No number at point"))
+      (replace-match
+       (number-to-string (+ p (string-to-number (match-string 0)))))
+    (move-to-column col)))
+(global-set-key (kbd "C-c +") 'increment-string-as-number)
+
 ;; emacsclient
 (server-start)
