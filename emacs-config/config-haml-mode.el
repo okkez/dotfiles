12828d8d193972aa8618b10e7c20f02f9f475bfa
okkez <okkez000@gmail.com>
2011-05-23 23:59:06 +0900

    update.

--- emacs-config/config-haml-mode.el
+++ emacs-config/config-haml-mode.el
@@ -1,6 +1,7 @@
 ;; haml-mode
 ;; git://github.com/nex3/haml.git
-(add-to-list 'load-path "~/.elisp/haml/extra")
+(add-to-list 'load-path "~/.elisp/haml-mode")
+(add-to-list 'load-path "~/.elisp/sass-mode")
 (require 'haml-mode)
 (require 'sass-mode)
 
