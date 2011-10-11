92174f3d6fdc1d787506cbf5b20eee85b1242757
okkez <okkez000@gmail.com>
2011-10-11 23:55:52 +0900

    remove unused configs.
    do not use lgrep.

--- emacs-config/config-base.el
+++ emacs-config/config-base.el
@@ -13,28 +13,7 @@
       ))
 ; 日本語 info が文字化けしないように
 (auto-compression-mode t)
-; xemacs の shell-mode で 日本語 EUC が使えるようにする
-(if (featurep 'xemacs)
-    (add-hook 'shell-mode-hook
-              (function
-               (lambda () (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)))))
-; 日本語 grep
-(if (file-exists-p "/usr/bin/lgrep")
-    (setq grep-command "lgrep -n "))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 漢字変換 (Anthy) の設定
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;(set-input-method "japanese-anthy")
-;(load-library "anthy")
-;(global-set-key "\C-\\" 'anthy-mode)
-;(setq default-input-method "japanese-anthy")
-;(toggle-input-method nil)
-;; UIM の設定
-;(require 'uim)
-;(require 'uim-leim)
-;(setq default-input-method "japanese-anthy-uim")
-;(global-set-key "\C-\\" 'uim-mode)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Xでのカラー表示
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
