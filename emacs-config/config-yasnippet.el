58d5289bba5ac786c0e5df74fff889e9ee79bb4f
okkez <okkez000@gmail.com>
2011-11-08 00:38:08 +0900

    add yasnippets-rails.

--- emacs-config/config-yasnippet.el
+++ emacs-config/config-yasnippet.el
@@ -1,7 +1,7 @@
 ;; yasnippet
-; git svn clone -rHEAD http://yasnippet.googlecode.com/svn/trunk/ yasnippet
+; git clone https://github.com/capitaomorte/yasnippet.git
 ; git clone git://github.com/eschulte/yasnippets-rails.git
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory "~/.elisp/yasnippets-rails/rails-snippets")
-(yas/load-directory "~/.elisp/yasnippet/snippets")
+(yas/load-directory "~/.emacs.d/el-get/yasnippets-rails/rails-snippets")
+(yas/load-directory "~/.emacs.d/el-get/yasnippet/snippets")
