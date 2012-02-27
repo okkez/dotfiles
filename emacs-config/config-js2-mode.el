7d5f7c43b4b96f37220188e07e6525e3bd033350
okkez <okkez000@gmail.com>
2012-02-28 00:48:20 +0900

    use https://github.com/mooz/js2-mode

--- emacs-config/config-js2-mode.el
+++ emacs-config/config-js2-mode.el
@@ -1,5 +1,6 @@
 ;; js2-mode
-(autoload 'js2-mode "js2" nil t)
+;;(autoload 'js2-mode "js2" nil t)
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook
           '(lambda ()
