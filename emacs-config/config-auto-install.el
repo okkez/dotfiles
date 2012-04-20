0283efc6c8f8368bc9d07678b55010e98264a896
Kenji Okimoto <kenji@genbu>
2012-04-20 22:53:06 +0900

    remove unused configs

--- emacs-config/config-auto-install.el
+++
@@ -1,6 +0,0 @@
-(require 'auto-install)
-(setq auto-install-directory "~/.elisp/repos/")
-(condition-case nil
-    (auto-install-update-emacswiki-package-name t)
-  (error (message "couldn't update package name, ignore error...")))
-(auto-install-compatibility-setup)
--- emacs-config/config-install-elisp.el
+++
@@ -1,4 +0,0 @@
-;; install-elisp
-(require 'install-elisp)
-(add-to-list 'load-path "~/.elisp/repos")
-(setq install-elisp-repository-directory "~/.elisp/repos/")
