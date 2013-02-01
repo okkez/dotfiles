41a1688c436f0b60bee67c5999bb7e73f5f11727
okkez <okkez000@gmail.com>
2013-02-01 23:14:28 +0900

    Add popwin.el

---
+++ emacs-config/config-popwin.el
@@ -0,0 +1,20 @@
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+(push "*Backtrace*" popwin:special-display-config)
+
+;; vc
+(push "*vc-diff*" popwin:special-display-config)
+(push "*vc-change-log*" popwin:special-display-config)
+
+;; helm
+(setq helm-samewindow nil)
+(push '("\\*[Hh]elm"
+        :regexp t
+        :position right
+        :width 100)
+      popwin:special-display-config)
+(push '("*my helm mini*"
+        :position right
+        :width 100)
+      popwin:special-display-config)
