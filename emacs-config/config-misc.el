befccd2d9e2685710273b62e3cc7fcca9ad835f1
okkez <okkez000@gmail.com>
2013-02-03 13:41:13 +0900

    Remove unused config

--- emacs-config/config-misc.el
+++ emacs-config/config-misc.el
@@ -103,10 +103,6 @@
 (define-key calendar-mode-map "n" 'calendar-forward-day)
 (define-key calendar-mode-map "b" 'calendar-backward-day)
 (setq mark-holidays-in-calendar t)
-;; (install-elisp "http://www.meadowy.org/meadow/netinstall/export/799/branches/3.00/pkginfo/japanese-holidays/japanese-holidays.el")
-(require 'japanese-holidays)
-(setq calendar-holidays
-      (append japanese-holidays local-holidays other-holidays))
 (setq calendar-weekend-marker 'diary)
 (add-hook 'today-visible-calendar-hook 'calendar-mark-weekend)
 (add-hook 'today-invisible-calendar-hook 'calendar-mark-weekend)
