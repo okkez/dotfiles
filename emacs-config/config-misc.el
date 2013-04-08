b4d52ae6f934608ca7bbeb1db72741e6410f1e0d
okkez <okkez000@gmail.com>
2013-04-08 22:24:31 +0900

    Remove config for grep-edit

--- emacs-config/config-misc.el
+++ emacs-config/config-misc.el
@@ -76,25 +76,6 @@
       '(("/Dropbox/*.*" . "~/.emacs.d/backup/")
         ))
 
-;; grep-edit
-;; http://d.hatena.ne.jp/rubikitch/20081025/1224869598
-(require 'grep)
-(require 'grep-edit)
-
-(defadvice grep-edit-change-file (around inhibit-read-only activate)
-  ""
-  (let ((inhibit-read-only t))
-    ad-do-it))
-;; (progn (ad-disable-advice 'grep-edit-change-file 'around 'inhibit-read-only) (ad-update 'grep-edit-change-file)) 
-
-(defun my-grep-edit-setup ()
-  (define-key grep-mode-map '[up] nil)
-  (define-key grep-mode-map "\C-c\C-c" 'grep-edit-finish-edit)
-  (message (substitute-command-keys "\\[grep-edit-finish-edit] to apply changes."))
-  (set (make-local-variable 'inhibit-read-only) t)
-  )
-(add-hook 'grep-setup-hook 'my-grep-edit-setup t)
-
 ;; racc-mode
 (autoload 'racc-mode "racc-mode" "alternate mode for editing racc files" t)
 (setq auto-mode-alist (append '(("\\.ry$" . racc-mode)) auto-mode-alist))
