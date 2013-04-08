4aa0b3d9c7eea6db6ec025379bd90b04da47bfa5
okkez <okkez000@gmail.com>
2013-04-08 23:36:18 +0900

    Remove rinari

--- emacs-config/config-rinari.el
+++
@@ -1,8 +0,0 @@
-;; Rinari
-; git clone git://github.com/eschulte/rinari.git
-; cd rinari
-; git submodule init
-; git submodule update
-(setq rinari-minor-mode-prefixes (list "r"))
-(require 'rinari)
-
