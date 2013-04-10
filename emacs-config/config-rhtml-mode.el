b335cbeb6197085afb4c0e3db1b0c65cf975de74
okkez <okkez000@gmail.com>
2013-04-10 14:55:07 +0900

    Remove needless hook to avoid an error

--- emacs-config/config-rhtml-mode.el
+++ emacs-config/config-rhtml-mode.el
@@ -1,7 +1,4 @@
 ;; rhtml-mode
 ; git clone git://github.com/eschulte/rhtml.git
-(add-to-list 'load-path "~/.elisp/rhtml")
 (require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-    (lambda () (rinari-launch)))
 
