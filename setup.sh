250c36eabe8453118963be78a0f5ffd127d8e817
okkez <okkez000@gmail.com>
2013-06-03 23:34:34 +0900

    Setup rbenv

--- setup.sh
+++ setup.sh
@@ -17,7 +17,9 @@ for f in $files; do
   fi
 done
 
-if test -d $HOME/.rbenv; then
+if ! test -d $HOME/.rbenv; then
   git clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
-  git clone git://github.com/sstephenson/ruby-build.git $HOME/.rben/plugins/ruby-build
+  git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+  git clone git://github.com/sstephenson/rbenv-gem-rehash.git $HOME/.rbenv/plugins/rbenv-gem-rehash
+  git clone git://github.com/sstephenson/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
 fi
