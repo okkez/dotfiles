bb4f557cfad31147f2693c358f516f342c58651b
Kenji Okimoto <okimoto@clear-code.com>
2013-08-06 13:05:06 +0900

    Do not popup helm window

--- emacs-config/config-popwin.el
+++ emacs-config/config-popwin.el
@@ -1,5 +1,6 @@
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
+(setq popwin:close-popup-window-timer-interval 0.05)
 
 (push "*Backtrace*" popwin:special-display-config)
 
@@ -8,20 +9,20 @@
 (push "*vc-change-log*" popwin:special-display-config)
 
 ;; helm
-(setq helm-samewindow nil)
-(push '("\\*[Hh]elm"
-        :regexp t
-        :position right
-        :width 100)
-      popwin:special-display-config)
-(push '("*my helm mini*"
-        :position right
-        :width 100)
-      popwin:special-display-config)
-
-;; po-mode
-(push '("\\*.*\\.po\\*"
-        :regexp t
-        :position bottom
-        :height 20)
-      popwin:special-display-config)
+;(setq helm-samewindow nil)
+;(push '("\\*[Hh]elm"
+;        :regexp t
+;        :position right
+;        :width 100)
+;      popwin:special-display-config)
+;(push '("*my helm mini*"
+;        :position right
+;        :width 100)
+;      popwin:special-display-config)
+;
+;;; po-mode
+;(push '("\\*.*\\.po\\*"
+;        :regexp t
+;        :position bottom
+;        :height 20)
+;      popwin:special-display-config)
