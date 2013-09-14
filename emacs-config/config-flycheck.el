9fcbb111fad096ae2eca99508d767df55ec6f985
okkez <okkez000@gmail.com>
2013-09-15 03:31:38 +0900

    Add missing config-flycheck.el

---
+++ emacs-config/config-flycheck.el
@@ -0,0 +1,4 @@
+(setq flycheck-display-errors-function
+      '(lambda (errors)
+         (-when-let (messages (-keep #'flycheck-error-message errors))
+           (popup-tip (s-join "\n\n" messages)))))
