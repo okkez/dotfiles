dafc860c7d36cbe8e7bd762f7788a005256aa97d
Kenji Okimoto <okimoto@clear-code.com>
2014-07-04 18:04:29 +0900

    zsh: Add peco-cdr

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -14,3 +14,12 @@ function peco-select-history() {
 zle -N peco-select-history
 bindkey '^r' peco-select-history
 
+function peco-cdr () {
+    local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-cdr
--- zshrc
+++ zshrc
@@ -155,6 +155,13 @@ autoload zed
 #autoload predict-on
 #predict-off
 
+## cdr - directory history
+#
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-max 5000
+zstyle ':chpwd:*' recent-dirs-default yes
+zstyle ':completion:*' recent-dirs-insert both
 
 ## Alias configuration
 #
