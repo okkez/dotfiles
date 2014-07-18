711d8767f998f03ebde3dfc15a7c2b23109b7dd3
okkez <okkez000@gmail.com>
2014-07-18 22:14:23 +0900

    Add peco-delete-history

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -14,6 +14,25 @@ function peco-select-history() {
 zle -N peco-select-history
 bindkey '^r' peco-select-history
 
+function peco-delete-history() {
+    local tac
+    local line
+    if which tac > /dev/null; then
+        tac="tac"
+    else
+        tac="tail -r"
+    fi
+    BUFFER=$(history -n 1 | \
+        eval $tac | \
+        peco --query "$LBUFFER")
+    line=$(grep -n -E ";${BUFFER}$" ${HISTFILE} | cut -d":" -f 1 | head -n 1)
+    sed -i.bak -e "${line}d" $HISTFILE
+    BUFFER=""
+    zle clear-screen
+}
+zle -N peco-delete-history
+bindkey '^x' peco-delete-history
+
 function peco-cdr () {
     local selected_dir=$(cdr -l | awk '{ print $2 }' | peco)
     if [ -n "$selected_dir" ]; then
