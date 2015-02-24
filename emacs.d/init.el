c98f8ce2f8a5d61b4fdeaaf482a91a70c1e0a4dc
okkez <okkez000@gmail.com>
2015-02-24 21:59:09 +0900

    Introduce emacs-jp/init-loader

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,20 @@
 ; -*- mode: elisp -*-
-(load "~/dotfiles/emacs-config/start.el")
+;; el-get は GitHub の master を使う
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
+;; additional recipes path
+(add-to-list 'el-get-recipe-path
+             (concat (file-name-directory load-file-name) "recipes"))
+
+(el-get-bundle! emacs-jp/init-loader
+  (setq-default init-loader-show-log-after-init t
+                init-loader-byte-compile t)
+  (init-loader-load
+   (concat (file-name-directory load-file-name) "emacs-config")))
