e91e15ebac8e6da241929365740c4b8afd64778d
Kenji Okimoto <okimoto@clear-code.com>
2015-06-10 11:56:10 +0900

    Arrange ace-isearch

--- emacs.d/init/ace-isearch.el
+++ emacs.d/init/ace-isearch.el
@@ -3,8 +3,8 @@
   (global-ace-isearch-mode +1)
 
   (custom-set-variables
-   '(ace-isearch-input-length 7)
-   '(ace-isearch-input-idle-delay 0.3)
+   '(ace-isearch-input-length 6)
+   '(ace-isearch-input-idle-delay 0.7)
    '(ace-isearch-submode 'ace-jump-char-mode)
    '(ace-isearch-use-ace-jump 'printing-char))
 
