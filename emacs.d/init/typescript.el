8c01515f90a876fb9feaff0d976f7b61e07a3ddc
Kenji Okimoto <okimoto@clear-code.com>
2015-08-10 16:53:44 +0900

    Set typescript-indent-level

--- emacs.d/init/typescript.el
+++ emacs.d/init/typescript.el
@@ -1,3 +1,4 @@
 (el-get-bundle tss
-  (add-to-list 'auto-mode-alist '("\.ts$" . typescript-mode)))
+  (add-to-list 'auto-mode-alist '("\.ts$" . typescript-mode))
+  (setq typescript-indent-level 2))
 
