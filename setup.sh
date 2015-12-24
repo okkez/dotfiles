e5f3c2c66b41db9223cdb6fbffd97f9e1620acd1
okkez <okkez000@gmail.com>
2015-12-24 23:18:43 +0900

    Use https:// instead of git://

--- setup.sh
+++ setup.sh
@@ -23,11 +23,11 @@ else
 fi
 
 if ! test -d $HOME/.rbenv; then
-  git clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
-  git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
-  git clone git://github.com/sstephenson/rbenv-gem-rehash.git $HOME/.rbenv/plugins/rbenv-gem-rehash
-  git clone git://github.com/sstephenson/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
-  git clone git://github.com/aripollak/rbenv-bundler-ruby-version.git $HOME/.rbenv/plugins/rbenv-bundler-ruby-version
+  git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+  git clone https://github.com/sstephenson/rbenv-gem-rehash.git $HOME/.rbenv/plugins/rbenv-gem-rehash
+  git clone https://github.com/sstephenson/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
+  git clone https://github.com/aripollak/rbenv-bundler-ruby-version.git $HOME/.rbenv/plugins/rbenv-bundler-ruby-version
 fi
 
 if ! dpkg -l golang > /dev/null 2>&1; then
