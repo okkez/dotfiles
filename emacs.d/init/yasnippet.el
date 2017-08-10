fa1298ca241e1081e084d087df6022736b73aff7
Kenji Okimoto <okimoto@clear-code.com>
2017-08-10 12:57:05 +0900

    Use yas-global-mode

--- emacs.d/init/yasnippet.el
+++ emacs.d/init/yasnippet.el
@@ -6,7 +6,7 @@
 (el-get-bundle helm-c-yasnippet
   (require 'yasnippet)
   (global-set-key (kbd "C-c y") 'helm-c-yas-complete)
-  (yas-initialize)
+  (yas-global-mode 1)
   (yas-load-directory "~/.emacs.d/el-get/yasnippets-rails/rails-snippets")
   (yas-load-directory "~/.emacs.d/el-get/yasnippet/snippets")
   (yas-load-directory "~/dotfiles/emacs.d/snippets"))
