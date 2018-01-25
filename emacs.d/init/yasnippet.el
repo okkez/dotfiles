918406a657c4e369a0be7e8dd4c330a7788ef9dd
Kenji Okimoto <okimoto@clear-code.com>
2018-01-25 12:26:17 +0900

    Use latest yasnippet

--- emacs.d/init/yasnippet.el
+++ emacs.d/init/yasnippet.el
@@ -2,12 +2,13 @@
 ; git clone https://github.com/capitaomorte/yasnippet.git
 ; git clone git://github.com/eschulte/yasnippets-rails.git
 
+(el-get-bundle yasnippet-snippets)
 (el-get-bundle yasnippets-rails)
 (el-get-bundle helm-c-yasnippet
   (require 'yasnippet)
   (global-set-key (kbd "C-c y") 'helm-c-yas-complete)
   (yas-global-mode 1)
   (yas-load-directory "~/.emacs.d/el-get/yasnippets-rails/rails-snippets")
-  (yas-load-directory "~/.emacs.d/el-get/yasnippet/snippets")
+  (yas-load-directory "~/.emacs.d/el-get/yasnippet-snippets/snippets/")
   (yas-load-directory "~/dotfiles/emacs.d/snippets"))
 
