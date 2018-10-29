0f17ac269f039ff8ef877c0517bf8f21f6f1d1ec
Kenji Okimoto <okimoto@clear-code.com>
2018-10-29 17:03:36 +0900

    Execute typescript-mode properly

--- emacs.d/init/typescript.el
+++ emacs.d/init/typescript.el
@@ -1,5 +1,5 @@
 (el-get-bundle tss
   :depends (json-mode)
+  (add-to-list 'auto-mode-alist '("\.ts$" . typescript-mode))
   (with-eval-after-load-feature 'tss
-    (add-to-list 'auto-mode-alist '("\.ts$" . typescript-mode))
     (setq typescript-indent-level 2)))
