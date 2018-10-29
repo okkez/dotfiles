327df9149936f6679b473892f90bef14dd67072c
Kenji Okimoto <okimoto@clear-code.com>
2018-10-29 17:22:17 +0900

    Indent

--- emacs.d/init/helm.el
+++ emacs.d/init/helm.el
@@ -4,62 +4,61 @@
   (require 'helm-buffers)
   (require 'helm-files)
   (require 'magit)
-(define-key global-map (kbd "C-;") 'helm-mini)
-(define-key global-map (kbd "M-x") 'helm-M-x)
-(define-key global-map (kbd "C-x C-f") 'helm-find-files)
-(define-key global-map (kbd "C-x b") 'helm-mini)
 
-(helm-mode +1)
-(helm-migemo-mode +1)
-(custom-set-variables
- '(helm-command-map-prefix-key "\C-z")
- '(helm-ff-smart-completion nil)
- '(helm-ff-auto-update-initial-value nil)
- '(helm-buffer-max-length 30)
- '(helm-mini-default-sources '(helm-source-buffers-list
-                               helm-source-recentf
-                               helm-source-files-in-current-dir
-                               helm-source-buffer-not-found)))
+  (define-key global-map (kbd "C-;") 'helm-mini)
+  (define-key global-map (kbd "M-x") 'helm-M-x)
+  (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+  (define-key global-map (kbd "C-x b") 'helm-mini)
 
-(define-key helm-find-files-map (kbd "C-h") nil)
-(define-key helm-map (kbd "C-h") nil)
-(set-face-background 'helm-selection "#104e8b")
-(set-face-foreground 'helm-source-header "gray")
+  (helm-mode +1)
+  (helm-migemo-mode +1)
+  (custom-set-variables
+   '(helm-command-map-prefix-key "\C-z")
+   '(helm-ff-smart-completion nil)
+   '(helm-ff-auto-update-initial-value nil)
+   '(helm-buffer-max-length 30)
+   '(helm-mini-default-sources '(helm-source-buffers-list
+                                 helm-source-recentf
+                                 helm-source-files-in-current-dir
+                                 helm-source-buffer-not-found)))
 
-(defvar helm-c-source-git-commit-messages
-  (helm-build-in-buffer-source "Git Commit Messages"
-    :init #'helm-c-git-commit-messages-init
-    :action (helm-make-actions
-             "Insert" (lambda (candidate)
-                        (insert
-                         (replace-regexp-in-string "\0" "\n" candidate))))
-    :real-to-display #'helm-c-git-commit-messages-real-to-display
-    :multiline t
-    :migemo t))
+  (define-key helm-find-files-map (kbd "C-h") nil)
+  (define-key helm-map (kbd "C-h") nil)
+  (set-face-background 'helm-selection "#104e8b")
+  (set-face-foreground 'helm-source-header "gray")
 
-(defun helm-git-commit-messages ()
-  "`helm' for git commit messages."
-  (interactive)
-  (helm-other-buffer
-   '(helm-c-source-git-commit-messages)
-   "*helm commit messages*"))
+  (defvar helm-c-source-git-commit-messages
+    (helm-build-in-buffer-source "Git Commit Messages"
+      :init #'helm-c-git-commit-messages-init
+      :action (helm-make-actions
+               "Insert" (lambda (candidate)
+                          (insert
+                           (replace-regexp-in-string "\0" "\n" candidate))))
+      :real-to-display #'helm-c-git-commit-messages-real-to-display
+      :multiline t
+      :migemo t))
 
-(defun helm-c-git-commit-messages-init ()
-  (with-temp-buffer
-    (call-process-shell-command
-     "git log --format=\"%x00%B\" | tr '\\n\\000\' '\\000\\n' | sed -e '/^$/d' -e 's/\\x0\\+$//'"
-     nil (current-buffer))
-    (helm-init-candidates-in-buffer 'global (buffer-string))))
+  (defun helm-git-commit-messages ()
+    "`helm' for git commit messages."
+    (interactive)
+    (helm-other-buffer
+     '(helm-c-source-git-commit-messages)
+     "*helm commit messages*"))
 
-(defun helm-c-git-commit-messages-real-to-display (candidate)
+  (defun helm-c-git-commit-messages-init ()
+    (with-temp-buffer
+      (call-process-shell-command
+       "git log --format=\"%x00%B\" | tr '\\n\\000\' '\\000\\n' | sed -e '/^$/d' -e 's/\\x0\\+$//'"
+       nil (current-buffer))
+      (helm-init-candidates-in-buffer 'global (buffer-string))))
+
+  (defun helm-c-git-commit-messages-real-to-display (candidate)
     (replace-regexp-in-string "\0" "\n" candidate))
 
-(defun magit-enable-helm ()
-  ;; 過去のコミットメッセージを挿入
-  (define-key git-commit-mode-map
-    (kbd "C-c i") 'helm-git-commit-messages))
-(add-hook 'magit-mode-hook
-          'magit-enable-helm)
-
-)
+  (defun magit-enable-helm ()
+    ;; 過去のコミットメッセージを挿入
+    (define-key git-commit-mode-map
+      (kbd "C-c i") 'helm-git-commit-messages))
+  (add-hook 'magit-mode-hook
+            'magit-enable-helm))
 
