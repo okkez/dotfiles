6f5c8d4503111b2c0eb8c404949a9cacd74c131a
okkez <okkez000@gmail.com>
2019-03-18 13:42:10 +0900

    emacs: Show kill ring

--- emacs.d/init/helm.el
+++ emacs.d/init/helm.el
@@ -9,6 +9,7 @@
   (define-key global-map (kbd "M-x") 'helm-M-x)
   (define-key global-map (kbd "C-x C-f") 'helm-find-files)
   (define-key global-map (kbd "C-x b") 'helm-mini)
+  (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 
   (helm-mode +1)
   (helm-migemo-mode +1)
