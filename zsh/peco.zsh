a8a59ad8d80cfe2352c60ae2bed3202011a0c0b1
okkez <okkez000@gmail.com>
2020-01-31 10:18:41 +0900

    Use bundle info --path

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -59,7 +59,7 @@ bindkey '^]' peco-ghq
 function cdgem() {
   local gem_name=$(bundle list | sed -e 's/^ *\* *//g' | peco | cut -d \  -f 1)
   if [ -n "$gem_name" ]; then
-    local gem_dir=$(bundle show ${gem_name})
+    local gem_dir=$(bundle info --path ${gem_name})
     echo "cd to ${gem_dir}"
     cd ${gem_dir}
   fi
