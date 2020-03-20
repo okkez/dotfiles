bd92359b73426f4c91a6cf5d75406496c01b3d82
okkez <okkez000@gmail.com>
2020-03-20 14:10:34 +0900

    set mmm-submode-decoration-level

--- emacs.d/init/vue-mode.el
+++ emacs.d/init/vue-mode.el
@@ -1,8 +1,7 @@
 ;; for eslint
 ;(el-get-bundle codesuki/add-node-modules-path)
 (el-get-bundle mmm-mode
-  '(mmm-global-mode (quote maybe) nil (mmm-mode))
-  '(mmm-submode-decoration-level 2))
+  (setq mmm-submode-decoration-level 2))
 (el-get-bundle elpa:edit-indirect)
 (el-get-bundle elpa:vue-html-mode)
 (el-get-bundle elpa:ssass-mode)
