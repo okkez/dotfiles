c9f326f7de6f8b57d6cacfd5bcf440b40f082841
okkez <okkez000@gmail.com>
2020-11-21 17:49:55 +0900

    Add other-window-or-split

--- emacs.d/init/misc.el
+++ emacs.d/init/misc.el
@@ -157,5 +157,32 @@ C-u 100 M-x increment-string-as-number ;; replaced by \"88\""
     (insert (format-time-string "%a %b %e %Y"))))
 (global-set-key (kbd "C-c d") 'insert-current-date)
 
+;; ウィンドウを n 等分に分割する
+(defun split-window-vertically-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-vertically)
+    (progn
+      (split-window-vertically
+       (- (window-height) (/ (window-height) num_wins)))
+      (split-window-vertically-n (- num_wins 1)))))
+(defun split-window-horizontally-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-horizontally)
+    (progn
+      (split-window-horizontally
+       (- (window-width) (/ (window-width) num_wins)))
+      (split-window-horizontally-n (- num_wins 1)))))
+
+(defun other-window-or-split ()
+  (interactive)
+  (when (one-window-p)
+    (if (>= (window-body-width) 270)
+        (split-window-horizontally-n 3)
+      (split-window-horizontally)))
+  (other-window 1))
+(global-set-key (kbd "<C-tab>") 'other-window-or-split)
+
 ;; emacsclient
 (server-start)
