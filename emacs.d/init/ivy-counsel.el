94c6b0c9587fa2f51ecb47dab37d49688a147d85
okkez <okkez000@gmail.com>
2020-11-21 17:49:13 +0900

    Add configurations related to ivy

--- emacs.d/init/ivy-counsel.el
+++ emacs.d/init/ivy-counsel.el
@@ -1,10 +1,24 @@
+(el-get-bundle amx)
+(el-get-bundle ivy-rich)
+(el-get-bundle ivy-posframe)
 (el-get-bundle counsel
+  (with-eval-after-load-feature 'ivy-rich
+    (ivy-rich-mode 1))
+  (with-eval-after-load-feature 'ivy-posframe
+    (setq ivy-posframe-display-functions-alist
+          '((counsel-M-x . ivy-posframe-display-at-point)
+            (counsel-yank-pop . ivy-posframe-display-at-point)
+            (t . ivy-posframe-display)
+            ))
+    (ivy-posframe-mode 1))
+
   (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
   (global-set-key (kbd "C-;") 'counsel-switch-buffer)
   (global-set-key (kbd "M-y") 'counsel-yank-pop)
 
-  (define-key counsel-find-file-map (kbd "C-l") 'counsel-up-directory)
+  (with-eval-after-load-feature 'counsel
+    (define-key counsel-find-file-map (kbd "C-l") 'counsel-up-directory))
 
   (with-eval-after-load "magit"
     (setq magit-completing-read-function 'ivy-completing-read))
