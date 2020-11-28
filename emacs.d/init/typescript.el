531b41df4f4f66a66c73c250c7e0c0505478cb8c
okkez <okkez000@gmail.com>
2020-11-28 23:56:21 +0900

    Stop using tss to stop installing auto-complete

--- emacs.d/init/typescript.el
+++ emacs.d/init/typescript.el
@@ -1,5 +1,7 @@
-(el-get-bundle tss
+(el-get-bundle typescript-mode
   :depends (json-mode)
-  (add-to-list 'auto-mode-alist '("\.ts$" . typescript-mode))
-  (with-eval-after-load-feature 'tss
+  (add-to-list 'auto-mode-alist
+               '("\.ts$" . typescript-mode)
+               '("\.tsx$" . typescript-mode))
+  (with-eval-after-load-feature 'typescript-mode
     (setq typescript-indent-level 2)))
