0a6637ac71b25b691b237d5990a3b3536ffd1254
okkez <okkez000@gmail.com>
2020-11-28 17:14:27 +0900

    Set variables for key-chord

--- emacs.d/init/key-chord.el
+++ emacs.d/init/key-chord.el
@@ -2,6 +2,8 @@
   :url https://github.com/zk-phi/key-chord.git
   :features key-chord
   (require 'key-chord)
-  (setq key-chord-two-keys-delay 0.04)
+  (setq key-chord-two-keys-delay 0.15
+        key-chord-safety-interval-backward 0.1
+        key-chord-safety-interval-forward  0.25)
   (key-chord-mode 1)
   (key-chord-define-global "jk" 'view-mode))
