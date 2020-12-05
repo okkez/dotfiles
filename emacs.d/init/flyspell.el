224b856f06e93ae5f320d551829371d5e15bf968
okkez <okkez000@gmail.com>
2020-12-06 00:58:26 +0900

    Use flyspell-correct

--- emacs.d/init/flyspell.el
+++ emacs.d/init/flyspell.el
@@ -1,68 +1,25 @@
 ;;; flyspell-mode
-(require 'flyspell)
-(require 'popup)
-  (with-eval-after-load 'popup
-    ;; Do no change M-TAB key bind
-    (setq flyspell-use-meta-tab nil)
+(el-get-bundle flyspell-correct-popup)
 
-    (define-key flyspell-mode-map (kbd "C-;") nil)
+(custom-set-variables
+ ;; Do no change M-TAB key bind
+ '(flyspell-use-meta-tab nil)
+ '(flyspell-auto-correct-binding (kbd "C-z C-;"))
+ '(ispell-program-name "aspell")
+ '(isqpell-dictionary "american"))
 
-    ;; Enable spell checker in text-mode
-    (mapc
-     (lambda (hook)
-       (add-hook hook 'flyspell-mode))
-     '(text-mode-hook))
-    ;; Enable spell checker in comment while programming
-    (mapc
-     (lambda (hook)
-       (add-hook hook 'flyspell-prog-mode))
-     '(prog-mode-hook
-       ruby-mode-hook))
+(with-eval-after-load-feature 'flyspell
+  (define-key flyspell-mode-map (kbd "C-1") 'flyspell-correct-wrapper))
 
-    (setq ispell-dictionary "american")
-
-    (defun flyspell-correct-word-popup-el ()
-      "Pop up a menu of possible corrections for misspelled word before point."
-      (interactive)
-      ;; use the correct dictionary
-      (flyspell-accept-buffer-local-defs)
-      (let ((cursor-location (point))
-            (word (flyspell-get-word nil)))
-        (if (consp word)
-            (let ((start (car (cdr word)))
-                  (end (car (cdr (cdr word))))
-                  (word (car word))
-                  poss ispell-filter)
-              ;; now check spelling of word.
-              (ispell-send-string "%\n") ;put in verbose mode
-              (ispell-send-string (concat "^" word "\n"))
-              ;; wait until ispell has processed word
-              (while (progn
-                       (accept-process-output ispell-process)
-                       (not (string= "" (car ispell-filter)))))
-              ;; Remove leading empty element
-              (setq ispell-filter (cdr ispell-filter))
-              ;; ispell process should return something after word is sent.
-              ;; Tag word as valid (i.e., skip) otherwise
-              (or ispell-filter
-                  (setq ispell-filter '(*)))
-              (if (consp ispell-filter)
-                  (setq poss (ispell-parse-output (car ispell-filter))))
-              (cond
-               ((or (eq poss t) (stringp poss))
-                ;; don't correct word
-                t)
-               ((null poss)
-                ;; ispell error
-                (error "Ispell: error in Ispell process"))
-               (t
-                ;; The word is incorrect, we have to propose a replacement.
-                (flyspell-do-correct (popup-menu* (car (cddr poss)) :scroll-bar t :margin t)
-                                     poss word cursor-location start end cursor-location)))
-              (ispell-pdict-save t)))))
-
-    ;; 修正したい単語の上にカーソルをもっていき, C-M-return を押すことで候補を選択
-    (add-hook 'flyspell-mode-hook
-              (lambda ()
-                (define-key flyspell-mode-map (kbd "C-1")
-                  'flyspell-correct-word-popup-el))))
+;; Enable spell checker in text-mode
+(mapc
+ (lambda (hook)
+   (add-hook hook 'flyspell-mode))
+ '(text-mode-hook))
+;; Enable spell checker in comment while programming
+(mapc
+ (lambda (hook)
+   (add-hook hook 'flyspell-prog-mode))
+ '(prog-mode-hook
+   enh-ruby-mode-hook
+   ruby-mode-hook))
