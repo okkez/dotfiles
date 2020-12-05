6e03ab469a230edb2270e555f8c4a40ad7eeff9c
okkez <okkez000@gmail.com>
2020-12-05 20:44:37 +0900

    Organize flycheck

--- emacs.d/init/flycheck.el
+++ emacs.d/init/flycheck.el
@@ -1,7 +1,5 @@
-(require 's)
 (el-get-bundle flycheck
-  (with-eval-after-load-feature 'flycheck
-    (setq flycheck-display-errors-function
-          '(lambda (errors)
-             (-when-let (messages (-keep #'flycheck-error-message errors))
-                        (popup-tip (s-join "\n\n" messages)))))))
+  (setq flycheck-display-errors-function
+        '(lambda (errors)
+           (-when-let (messages (-keep #'flycheck-error-message errors))
+             (popup-tip (s-join "\n\n" messages))))))
