ded22a5b0d07bf7f3479d5e9627f4665405ab3c9
okkez <okkez000@gmail.com>
2020-12-12 13:53:35 +0900

    Remove company-lsp

--- emacs.d/init/company-mode.el
+++ emacs.d/init/company-mode.el
@@ -37,7 +37,3 @@
 
 (el-get-bundle company-emoji
   (add-to-list 'company-backends 'company-emoji))
-
-(el-get-bundle tigersoldier/company-lsp
-  (with-eval-after-load-feature 'company-mode
-    (add-to-list 'company-backends 'company-lsp)))
