0cc99c48168ad75dd51b7ca93e2837c663cbad07
okkez <okkez000@gmail.com>
2020-12-13 18:01:57 +0900

    Organize org-mode configuration

--- emacs.d/init/org.el
+++ emacs.d/init/org.el
@@ -12,8 +12,31 @@
    ;; TODO の状態
    '(org-todo-keywords '("TODO" "Wait" "DONE"))
    '(org-todo-interpretation 'sequence)
-   )
+   ;; Capture templates for code-reading
+   '(org-capture-templates
+     '(("code-link"
+        "Store the code-reading notes with GitHub and file links for the current cursor position."
+        plain
+        (function org-code-capture--find-store-point)
+        "%^{Summary}\n%(with-current-buffer (org-capture-get :original-buffer) (browse-at-remote-get-url))\n# %a"
+        :immediate-finish t)
+       ("just-code-link"
+        "Immediately store GitHub and file links for the current cursor position to the current code-reading notes."
+        plain
+        (function org-code-capture--find-store-point)
+        "%(with-current-buffer (org-capture-get :original-buffer) (browse-at-remote-get-url))\n# %a"
+        :immediate-finish t)
+       ("t" "Todo" entry (file+headline org-default-notes-file "Inbox")
+        "** TODO %?\n   %i\n   %a\n   %t")
+       ("b" "Bug" entry (file+headline org-default-notes-file "Inbox")
+        "** TODO %?   :bug:\n   %i\n   %a\n   %t")
+       ("i" "Idea" entry (file+headline org-default-notes-file "New Ideas")
+        "** %?\n   %i\n   %a\n   %t"))))
+
   (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+  (add-hook 'org-mode-hook '(lambda ()
+                              (org-bullets-mode 1)
+                              (key-chord-define-local "cx" 'org-toggle-checkbox)))
 
   ;;; start https://ladicle.com/post/20200625_123915/
   (defconst okkez/org-journal-dir "~/Nextcloud/org/journal/")
@@ -53,91 +76,23 @@
     (org-end-of-subtree))
   ;;; end https://ladicle.com/post/20200625_123915/
 
-  ;; Capture templates for code-reading
-  (setq org-capture-templates
-        '(("code-link"
-           "Store the code-reading notes with GitHub and file links for the current cursor position."
-           plain
-           (function org-code-capture--find-store-point)
-           "%^{Summary}\n%(with-current-buffer (org-capture-get :original-buffer) (browse-at-remote-get-url))\n# %a"
-           :immediate-finish t)
-          ("just-code-link"
-           "Immediately store GitHub and file links for the current cursor position to the current code-reading notes."
-           plain
-           (function org-code-capture--find-store-point)
-           "%(with-current-buffer (org-capture-get :original-buffer) (browse-at-remote-get-url))\n# %a"
-           :immediate-finish t)
-          ("t" "Todo" entry (file+headline org-default-notes-file "Inbox")
-           "** TODO %?\n   %i\n   %a\n   %t")
-          ("b" "Bug" entry (file+headline org-default-notes-file "Inbox")
-           "** TODO %?   :bug:\n   %i\n   %a\n   %t")
-          ("i" "Idea" entry (file+headline org-default-notes-file "New Ideas")
-           "** %?\n   %i\n   %a\n   %t")
-          ))
-
-  ;; for link
-  (defun org-next-visible-link ()
-    "Move forward to the next link. If the link is in hidden text, expose it."
-    (interactive)
-    (when (and org-link-search-failed (eq this-command last-command))
-      (goto-char (point-min))
-      (message "Link search wrapped back to beginning of buffer"))
-    (setq org-link-search-failed nil)
-    (let* ((pos (point))
-           (ct (org-context))
-           (a (assoc :link ct))
-           srch)
-      (if a (goto-char (nth 2 a)))
-      (while (and (setq srch (re-search-forward org-any-link-re nil t))
-                  (goto-char (match-beginning 0))
-                  (prog1 (not (eq (org-invisible-p) 'org-link))
-                    (goto-char (match-end 0)))))
-      (if srch
-          (goto-char (match-beginning 0))
-        (goto-char pos)
-        (setq org-link-search-failed t)
-        (error "No further link found"))))
-
-  (defun org-previous-visible-link ()
-    "Move backward to the previous link. If the link is in hidden text, expose it."
-    (interactive)
-    (when (and org-link-search-failed (eq this-command last-command))
-      (goto-char (point-max))
-      (message "Link search wrapped back to end of buffer"))
-    (setq org-link-search-failed nil)
-    (let* ((pos (point))
-           (ct (org-context))
-           (a (assoc :link ct))
-           srch)
-      (if a (goto-char (nth 1 a)))
-      (while (and (setq srch (re-search-backward org-any-link-re nil t))
-                  (goto-char (match-beginning 0))
-                  (not (eq (org-invisible-p) 'org-link))))
-      (if srch
-          (goto-char (match-beginning 0))
-        (goto-char pos)
-        (setq org-link-search-failed t)
-        (error "No further link found"))))
-
-  (define-key org-mode-map (kbd "M-n") 'org-next-visible-link)
-  (define-key org-mode-map (kbd "M-p") 'org-previous-visible-link)
-
   ;; agenda
   (define-key global-map (kbd "C-c a") 'org-agenda)
 
   ;; key-chord
   ; (key-chord-define-global "op" 'org-remember)
 
-  ;; ~/memo/agenda.org を開く
-  (define-key global-map (kbd "C-z a")
-    '(lambda () (interactive) (find-file "~/Nextcloud/memo/agenda.org")))
-  ;; ~/Nextcloud/memo/standup-meeting.org を開く
-  (define-key global-map (kbd "C-z s")
-    '(lambda () (interactive) (find-file "~/Nextcloud/memo/standup-meeting.org")))
-  ;; ~/Nextcloud/memo/cheat-sheet.org を開く
-  (define-key global-map (kbd "C-z c")
-    '(lambda () (interactive) (find-file "~/Nextcloud/memo/cheat-sheet.org")))
   )
+
+;; ~/memo/agenda.org を開く
+(define-key global-map (kbd "C-z a")
+  '(lambda () (interactive) (find-file "~/Nextcloud/memo/agenda.org")))
+;; ~/Nextcloud/memo/standup-meeting.org を開く
+(define-key global-map (kbd "C-z s")
+  '(lambda () (interactive) (find-file "~/Nextcloud/memo/standup-meeting.org")))
+;; ~/Nextcloud/memo/cheat-sheet.org を開く
+(define-key global-map (kbd "C-z c")
+  '(lambda () (interactive) (find-file "~/Nextcloud/memo/cheat-sheet.org")))
 
 ;; 祝日を表示する 不要？
 ;(setq org-agenda-files (list org-default-notes-file
