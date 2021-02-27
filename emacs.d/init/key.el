e971e569f1836276ce01fc32237b87bd940393cb
okkez <okkez000@gmail.com>
2021-02-27 15:49:51 +0900

    Use kbd macro

--- emacs.d/init/key.el
+++ emacs.d/init/key.el
@@ -7,13 +7,13 @@
 (global-set-key "\C-m" 'newline-and-indent)
 
 ;; バッファ全体をインデントする
-(global-set-key "\C-x\C-i"
+(global-set-key (kbd "C-x C-i")
                 '(lambda ()
                    (interactive)
                    (indent-region (point-min) (point-max) nil)))
 
 ;; other-window を逆回りにする
-(global-set-key "\C-x\p"
+(global-set-key (kbd "C-x p")
                 '(lambda ()
                    (interactive)
                    (other-window -1)))
