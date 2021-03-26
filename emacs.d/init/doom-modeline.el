486188ee023c2c58f00f2f067f4c714a50cf1ee9
okkez <okkez000@gmail.com>
2021-03-26 10:21:35 +0900

    Use doom-modeline-buffer-file-name-style 'auto

--- emacs.d/init/doom-modeline.el
+++ emacs.d/init/doom-modeline.el
@@ -1,6 +1,6 @@
 (el-get-bundle doom-modeline
   (custom-set-variables
-   '(doom-modeline-buffer-file-name-style 'truncate-with-project)
+   '(doom-modeline-buffer-file-name-style 'auto)
    '(doom-modeline-buffer-modification-icon nil)
    '(doom-modeline-minor-modes nil))
 
