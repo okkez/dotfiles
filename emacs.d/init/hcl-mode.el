e15aec6e3635ab6aa519024fd5499869aa3574cc
okkez <okkez000@gmail.com>
2021-03-26 10:22:04 +0900

    Use hcl-mode properly

--- emacs.d/init/hcl-mode.el
+++ emacs.d/init/hcl-mode.el
@@ -1,5 +1,3 @@
 ;; hcl-mode
 (el-get-bundle hcl-mode
-  (with-eval-after-load-feature 'hcl-mode
-    (add-to-list 'auto-mode-alist '("\\.tf$" . hcl-mode))
-    ))
+    (add-to-list 'auto-mode-alist '("\\.tf$" . hcl-mode)))
