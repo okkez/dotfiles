513fd6e95d2caa022434e0ebc4a926605ea0adb9
okkez <okkez000@gmail.com>
2021-04-10 16:47:27 +0900

    Add key bind for browse-at-remote

--- emacs.d/init/browse-at-remote.el
+++ emacs.d/init/browse-at-remote.el
@@ -1,2 +1,3 @@
 (el-get-bundle browse-at-remote
-  (global-set-key (kbd "M-g r") 'browse-at-remote))
+  (global-set-key (kbd "M-g r") 'browse-at-remote)
+  (global-set-key (kbd "M-g k") 'browse-at-remote-kill))
